7af6f00a3def6d834ad9daa0ab8d7b5538916cb6
Rozena Akter <153695774+rozenaakter@users.noreply.github.com>
2024-10-24 23:22:58 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    const fontSizeInput = document.getElementById('fontSize');
+    const textToResize = document.getElementById('textToResize');
+
+    fontSizeInput.addEventListener('input', function () {
+        const newSize = fontSizeInput.value + 'px';
+        textToResize.style.fontSize = newSize;
+        textToResize.style.color = 'red';
+    });
+});
